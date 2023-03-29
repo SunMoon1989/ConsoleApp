5b99d43dc3aab072dcbeef4dec3711be39232ac2
My <My@DESKTOP-RV9MVQS>
2023-03-29 12:26:01 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        int sk1;
+        static void Main(string[] args)
+        {
+            /*  int a = 1;
+              int b = 5;
+              Console.WriteLine("Hello World!");
+              Program sn = new Program();
+             int c= sn.add(a, b);
+              Console.WriteLine("Hello World!1" + sn.sk1) ;
+              Console.WriteLine("Hello World!" +c);
+
+              sn.Name = "Shanm";
+              Console.WriteLine(sn.Name);
+              Console.WriteLine("Hello World!" + sn.sk1);
+              Console.WriteLine("Hello World!" + sn.sk1);
+             // double sqrtNum = Math.Sqrt(16);
+              Console.WriteLine(Math.Sqrt(16));
+              Console.WriteLine(Math.PI);
+              Console.WriteLine(Math.Pow(2,3));
+              Console.WriteLine(Math.Abs(5.4));
+              Console.WriteLine(Math.Ceiling(3.5));
+              Console.WriteLine(Math.Floor(3.5));
+              Console.WriteLine(Math.Round(3.5));
+              Console.WriteLine(Math.Round(3.6));
+              Console.WriteLine(Math.Round(3.4));
+              Console.WriteLine(Math.Max(3,5));
+              Console.WriteLine(Math.Truncate(4.6));
+
+              Console.WriteLine(string.Concat("shan","muga"));
+              Console.WriteLine(string.Compare("sh1","sh"));
+              Console.WriteLine(string.Join("-","bas","sd"));*/
+
+            sncls1 s = new sncls1();
+            s.snadd1();
+
+            string s1 = "hello world";
+            string capitalized = StringExtensions.CapitalizeFirstLetter(s1);
+            string capitalized1 = s1.CapitalizeFirstLetter();
+            Console.WriteLine(capitalized+" "+ capitalized1);
+            string capitalized2 = StringExtensions.CapitalizeFirstLetter(s1);
+            Console.WriteLine(capitalized2);
+        }
+
+        private string name;
+        public string Name
+        {
+            get { 
+                return name;
+            }
+            set
+            {
+
+                name = value;
+                name = "sk";
+                sk1 = 5;
+            }
+        }
+     
+
+        public int add(int a, int b)
+        {
+            Console.WriteLine("Hello World!"+a);
+            sk1 = 2;
+            return a + b+sk1;
+            
+        }
+    }
+}
---
+++ ConsoleApp/sncls.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class sncls 
+    {
+        public sncls()
+        {
+            Console.WriteLine("sncls");
+        }
+        public void snadd()
+        {
+            Console.WriteLine("snadd");
+        }
+    }
+    class sncls1
+    {
+        public sncls1()
+        {
+            Console.WriteLine("sncls1");
+             snscls.snsadd();
+            snslcls seld = new snslcls();
+            seld.snsadd();
+
+           // Console.WriteLine(obj.GetType());
+
+        }
+        public void snadd1()
+        {
+            Console.WriteLine("snadd");
+        }
+            }
+
+    static class snscls
+    {
+        public static void snsadd()
+        {
+            Console.WriteLine("snsadd");
+        }
+    }
+
+    sealed class snslcls :sncls
+    {
+        public void snsadd()
+        {
+            Console.WriteLine("sealdsadd");
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string CapitalizeFirstLetter(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return str;
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+    }
+
+    sealed class SealedClass
+    {
+        public static string CapitalizeFirstLetter( string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return str;
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+    }
+}
